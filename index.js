5d8fc059ecca9ae9fa2f0bae1e14b16a8a43cc13
pllehas95 <110645471+pllehas95@users.noreply.github.com>
2022-08-04 22:39:19 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const AmigosBtn = document.querySelector('#AmigosBtn');
+
+AmigosBtn.addEventListener('click',function () {
+    alert('Excelente Decisión')
+});
+
+const AmigosconBeneficiosBtn = document.querySelector('#AmigosconBeneficiosBtn');
+
+AmigosconBeneficiosBtn.addEventListener('click', function () {
+    alert('Lo sabía')
+   
+});
+
+const ParejaBtn = document.querySelector('#ParejaBtn');
+
+ParejaBtn.addEventListener('click', function () {
+alert('Lo siento ya tengo, pero Gracias por tener buen gusto');
+
+})
